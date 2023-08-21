6d7051f55323f51a4946830b7da05b17b431052a
cosmiclasagnadev <allenpdl75@gmail.com>
2023-08-22 03:09:08 +0800

    cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({subsets: ['latin']})
 export const metadata: Metadata = {
   title: 'Free AI Tools Library | AiTools',
   description: 'Curated collection of AI tools, all in one place. Discover the perfect tool for your next project or business and speed up your workflow!',
+  icons: {
+    icon: '/favicon.png',
+  },
 }
 
 export default function RootLayout({
