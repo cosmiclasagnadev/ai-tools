f87a5b46b60410b2644bbd5a5b6888c0ebe5f808
cosmiclasagnadev <allenpdl75@gmail.com>
2025-01-31 15:26:27 +0800

    move to vercel analytics free tier and removed g-analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,65 +1,62 @@
-import MainSideNav from '@/components/layouts/MainSideNav'
-import './globals.css'
-import type {Metadata} from 'next'
-import {Plus_Jakarta_Sans} from 'next/font/google'
-import Script from 'next/script'
-import {Suspense} from 'react'
-import Analytics from '@/components/Analytics'
+import MainSideNav from "@/components/layouts/MainSideNav";
+import "./globals.css";
+import type { Metadata } from "next";
+import { Plus_Jakarta_Sans } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
 
-const inter = Plus_Jakarta_Sans({subsets: ['latin']})
+const inter = Plus_Jakarta_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'Free AI Tools Library | AiTools',
-  description: 'Curated collection of AI tools, all in one place. Discover the perfect tool for your next project or business and speed up your workflow!',
-  keywords: ['AI Tools', 'Free AI Tools', 'Free AI Tools Library'],
-  authors: [{name: 'Allen Ponce de Leon'}],
-  creator: 'Allen Ponce de Leon',
+  title: "Free AI Tools Library | AiTools",
+  description:
+    "Curated collection of AI tools, all in one place. Discover the perfect tool for your next project or business and speed up your workflow!",
+  keywords: ["AI Tools", "Free AI Tools", "Free AI Tools Library"],
+  authors: [{ name: "Allen Ponce de Leon" }],
+  creator: "Allen Ponce de Leon",
   icons: {
-    icon: '/favicon.png',
+    icon: "/favicon.png",
   },
   alternates: {
-    canonical: 'https://aitools.sh',
+    canonical: "https://aitools.sh",
     languages: {
-      'en-US': '/en-US',
+      "en-US": "/en-US",
     },
   },
   openGraph: {
-    type: 'website',
-    url: 'https://aitools.sh',
-    title: 'Free AI Tools Library | AiTools',
-    description: 'Curated collection of AI tools, all in one place. Discover the perfect tool for your next project or business and speed up your workflow!',
+    type: "website",
+    url: "https://aitools.sh",
+    title: "Free AI Tools Library | AiTools",
+    description:
+      "Curated collection of AI tools, all in one place. Discover the perfect tool for your next project or business and speed up your workflow!",
     images: {
-      url: '/opengraph-image.jpg',
-      alt: 'Free AI Tools Library | AiTools',
+      url: "/opengraph-image.jpg",
+      alt: "Free AI Tools Library | AiTools",
     },
   },
   twitter: {
-    creator: '@cosmiclasagnadev',
-    card: 'summary_large_image',
-    title: 'Free AI Tools Library | AiTools',
-    description: 'Curated collection of AI tools, all in one place. Discover the perfect tool for your next project or business and speed up your workflow!',
+    creator: "@cosmiclasagnadev",
+    card: "summary_large_image",
+    title: "Free AI Tools Library | AiTools",
+    description:
+      "Curated collection of AI tools, all in one place. Discover the perfect tool for your next project or business and speed up your workflow!",
     images: {
-      url: '/opengraph-image.jpg',
-      alt: 'Free AI Tools Library | AiTools',
+      url: "/opengraph-image.jpg",
+      alt: "Free AI Tools Library | AiTools",
     },
-  }
-}
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Suspense>
-          <Analytics />
-        </Suspense>
-        <MainSideNav>
-          {children}
-        </MainSideNav>
+        <MainSideNav>{children}</MainSideNav>
+        <Analytics />
       </body>
     </html>
-  )
+  );
 }
