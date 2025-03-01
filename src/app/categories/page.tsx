d7484cdbdac1887ddb9ebb0808bb01efa6958d7e
Allen <allenpdl75@gmail.com>
2025-03-01 16:59:40 +0800

    move to vercel analytics free tier and removed g-analytics (#2)
    * move to vercel analytics free tier and removed g-analytics
    
    * build errors

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -1,51 +1,56 @@
-import React from 'react'
-import {CATEGORIES_CONTENT} from '@/constants/Content'
-import supabase from '@/lib/supabase'
-import ContentAreaLayout from '@/components/layouts/ContentAreaLayout'
-import CategoriesContentArea from '@/components/CategoriesContentArea'
+import React from "react";
+import { CATEGORIES_CONTENT } from "@/constants/Content";
+import supabase from "@/lib/supabase";
+import ContentAreaLayout from "@/components/layouts/ContentAreaLayout";
+import CategoriesContentArea from "@/components/CategoriesContentArea";
 
-type Props = {}
+type Props = {};
 
 export const revalidate = 30;
 
 export const metadata = {
-    title: 'AI Tool Categories | AI Tools',
-    description: 'Curated collection of AI tools, all in one place. Discover the perfect tool for your next project or business and speed up your workflow!',
-    openGraph: {
-        title: 'AI Tool Categories | AI Tools',
-        url: 'https://aitools.sh',
-        description: 'Curated collection of AI tools, all in one place. Discover the perfect tool for your next project or business and speed up your workflow!',
-        type: 'website',
-        images: {
-            url: '/opengraph-image.jpg',
-            alt: 'Free AI Tools Library | AiTools',
-        },
+  metadataBase: new URL("https://aitools.sh"),
+  title: "AI Tool Categories | AI Tools",
+  description:
+    "Curated collection of AI tools, all in one place. Discover the perfect tool for your next project or business and speed up your workflow!",
+  openGraph: {
+    title: "AI Tool Categories | AI Tools",
+    url: "https://aitools.sh",
+    description:
+      "Curated collection of AI tools, all in one place. Discover the perfect tool for your next project or business and speed up your workflow!",
+    type: "website",
+    images: {
+      url: "/opengraph-image.jpg",
+      alt: "Free AI Tools Library | AiTools",
     },
-    twitter: {
-        title: 'AI Tool Categories | AI Tools',
-        creator: '@cosmiclasagnadev',
-        card: 'summary_large_image',
-        site: 'https://aitools.sh',
-        description: 'Curated collection of AI tools, all in one place. Discover the perfect tool for your next project or business and speed up your workflow!',
-        type: 'website',
+  },
+  twitter: {
+    title: "AI Tool Categories | AI Tools",
+    creator: "@cosmiclasagnadev",
+    card: "summary_large_image",
+    site: "https://aitools.sh",
+    description:
+      "Curated collection of AI tools, all in one place. Discover the perfect tool for your next project or business and speed up your workflow!",
+    type: "website",
+  },
+  alternates: {
+    canonical: "https://aitools.sh/categories",
+    languages: {
+      "en-US": "/en-US",
     },
-    alternates: {
-        canonical: 'https://aitools.sh/categories',
-        languages: {
-            'en-US': '/en-US',
-        },
-    },
-}
-
-
+  },
+};
 
 const CategoriesPage = async (props: Props) => {
-    const {data: categories} = await supabase.from('tools_categ').select('*');
-    return (
-        <ContentAreaLayout>
-            <CategoriesContentArea content={CATEGORIES_CONTENT} categories={categories} />
-        </ContentAreaLayout>
-    )
-}
+  const { data: categories } = await supabase.from("tools_categ").select("*");
+  return (
+    <ContentAreaLayout>
+      <CategoriesContentArea
+        content={CATEGORIES_CONTENT}
+        categories={categories}
+      />
+    </ContentAreaLayout>
+  );
+};
 
-export default CategoriesPage
+export default CategoriesPage;
